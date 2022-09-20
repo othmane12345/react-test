66efc5f02f8c196ec8242c7fa55e548c2be4f9bc
othmane12345 <34275484+othmane12345@users.noreply.github.com>
2022-09-20 09:15:14 +0100

    [feature]: login refactoring

--- src/core/components/search-bar/search-bar.js
+++ src/core/components/search-bar/search-bar.js
@@ -1,10 +1,29 @@
-import { Input } from "antd";
+import { Input, Popover, List } from "antd";
+import { useState } from "react";
+
 const { Search } = Input;
 
 function SearchBar() {
+    const [open, setOpen] = useState(false);
+    const [search, setSearch] = useState('');
+
+    const content = (
+        <div>
+            <List>
+                
+            </List>
+        </div>
+    )
+
+    function searchGlobal() {
+
+    }
+
     return (
         <div>
-            <Search className="search-input" placeholder="Search..." />
+            <Search className="search-input" placeholder="Search..." value={search}/>
+            <Popover placement="topLeft" content={content} open={open}>
+            </Popover>
         </div>
     );
 }
--- src/main/containers/login/login.js
+++ src/main/containers/login/login.js
@@ -1,29 +1,29 @@
 import './login.scss';
-import { Space, Input, Button, Typography } from 'antd';
-import { useState } from 'react';
+import { Space, Input, Button, Typography, Form, notification } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import { connectUser } from '../../store/users/user-slice';
+import { initAllPosts } from '../../store/posts/post-slice';
 import { useNavigate } from 'react-router-dom';
 const { Title } = Typography;
 
 function Login() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
 
-    function handleChangeUserName(newValue) {
-        setUserName(newValue)
-    }
-
-    function handleChangePassword(newValue) {
-        setPassword(newValue)
-    }
-
-    function connect() {
-        dispatch(connectUser({userName, password}));
+    const onFinish = (values) => {
+        dispatch(connectUser(values));
+        dispatch(initAllPosts())
         navigate('/Home')
-    }
+    };
+    
+    const onFinishFailed = (errorInfo) => {
+        notification.error({
+            message: 'Error',
+            description:
+              `An error occurred while trying to connect please make sure you input the right informations!`,
+            placement: 'bottomRight'
+          });
+    };
 
     return (
         <div className="main-wrap">
@@ -33,12 +33,36 @@ function Login() {
                 <Space direction="vertical" size="large" style={{width: '81%'}}>
                     <Title level={3}>Sign In</Title>
 
-                    <Space direction="vertical" size="middle" style={{width: '100%'}}>
-                        <Input placeholder='email or username *' value={userName} onChange={(event) => handleChangeUserName(event.target.value)} />
-                        <Input.Password placeholder='password *' value={password} onChange={(event) => handleChangePassword(event.target.value)} />
-                    </Space>
+                    <Form className='form'
+                        name="basic"
+                        wrapperCol={{xs: {span: 24}, sm: {span: 24}}}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item
+                        name="userName"
+                        rules={[{
+                            required: true,
+                            message: 'Please input your username!',
+                        }]}>
+                            <Input placeholder='email or username *' />
+                        </Form.Item>
 
-                    <Button type="primary" onClick={() => connect()}>Login</Button>
+                        <Form.Item
+                        name="password"
+                        rules={[{
+                            required: true,
+                            message: 'Please input your password!',
+                        }]}
+                        >
+                            <Input.Password placeholder='password *' />
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" block>Login</Button>
+                        </Form.Item>
+                    </Form>
                 </Space>
             </div>
         </div>
